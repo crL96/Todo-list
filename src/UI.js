a5811dd8d68f0040dc78f5956ed8c9c33c491675
crL96 <190506813+crL96@users.noreply.github.com>
2025-02-04 14:33:07 +0100

    Add function for rendering all tasks in a project to DOM

--- src/UI.js
+++ src/UI.js
@@ -30,4 +30,58 @@ const sidebarUI = (function() {
     return {renderProjectList};
 })();
 
-export { sidebarUI };
+
+const contentUI = (function() {
+
+    function renderProject(index) {
+        const container = document.querySelector("#projectContainer");
+        container.replaceChildren();
+        const currentProject = projects.list[index];
+    
+        for (let task of currentProject.tasks) {
+            const taskContainer = document.createElement("div");
+            taskContainer.classList.add("task");
+    
+            const name = document.createElement("h3");
+            name.textContent = task.name;
+            taskContainer.appendChild(name);
+    
+            const desc = document.createElement("p");
+            desc.textContent = task.desc;
+            taskContainer.appendChild(desc);
+    
+            const dueDate = document.createElement("p");
+            dueDate.textContent = task.dueDate;
+            taskContainer.appendChild(dueDate);
+    
+            const prio = document.createElement("p");
+            prio.textContent = task.prio;
+            taskContainer.appendChild(prio);
+    
+            const checkTitle = document.createElement("span");
+            checkTitle.textContent = "Completed:"
+            const check = document.createElement("input");
+            check.setAttribute("type", "checkbox");
+            check.textContent = task.doneStatus;
+            taskContainer.appendChild(checkTitle);
+            taskContainer.appendChild(check);
+            
+            const deleteBtn = document.createElement("button");
+            deleteBtn.classList.add("deleteTask");
+            deleteBtn.textContent = "Delete";
+            taskContainer.appendChild(deleteBtn);
+    
+            const editBtn = document.createElement("button");
+            editBtn.classList.add("editTask");
+            editBtn.textContent = "Edit";
+            taskContainer.appendChild(editBtn);
+            
+            container.appendChild(taskContainer);
+        }
+    }
+
+    return {renderProject};
+})();
+
+
+export { sidebarUI, contentUI };
