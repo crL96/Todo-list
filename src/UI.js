28315b8702a843dd194854491fc5b3fec37ffe68
crL96 <190506813+crL96@users.noreply.github.com>
2025-02-04 12:59:08 +0100

    Add button with function to add new project

---
+++ src/UI.js
@@ -0,0 +1,33 @@
+import { allProjects } from "./projects.js";
+
+const sidebarUI = (function() {
+
+    function renderProjectList(allProjects) {
+        const projectList = document.querySelector("#projectList");
+        projectList.replaceChildren();
+
+        for (let project of allProjects) {
+            const projectListItem = document.createElement("li");
+            const projectTitle = document.createElement("button");
+            projectTitle.textContent = project.name;
+            projectListItem.appendChild(projectTitle);
+            projectList.appendChild(projectListItem);
+        }
+    }
+
+    // New Project Button
+    const btnAddProject = document.querySelector("#btnAddProject");
+    btnAddProject.addEventListener("click", () => {
+        const ElName = document.getElementById("projectName");
+        const valueName = ElName.value;
+        if (!(valueName === "")) {
+            allProjects.addNewProject(valueName.trim());
+            ElName.value = "";
+            sidebarUI.renderProjectList(allProjects.projectList);
+        }
+    });
+
+    return {renderProjectList};
+})();
+
+export { sidebarUI };
