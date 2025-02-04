513025ed5d0c85ba67db17804e46b7e9baaaa261
crL96 <190506813+crL96@users.noreply.github.com>
2025-02-04 12:54:10 +0100

    Add allProjects object in projects module

--- src/projects.js
+++ src/projects.js
@@ -1,17 +1,32 @@
 import Task from "./tasks.js";
 
-export default class Project {
+class Project {
     tasks = [];
-
+    
     constructor(name) {
         this.name = name;
     }
-
+    
     addTask(name, desc, dueDate, prio) {
         this.tasks.push(new Task(name, desc, dueDate, prio));
     }
-
+    
     deleteTask(index) {
         this.tasks.splice(index, 1);
     }
 }
+
+const allProjects = (function(){
+    const projectList = [];
+
+    function addNewProject(name) {
+        projectList.push(new Project(name));
+    }
+
+    return {addNewProject, projectList};
+})();
+
+allProjects.addNewProject("Default project");
+
+
+export { Project, allProjects };
